d49d721a06fcf83c842b26c89d34bd768ed89dba
ja153903 <jaimeabbariao@gmail.com>
2023-01-24 01:43:23 -0500

    feat: including stub for new component

---
+++ src/components/FlagsAutoComplete/FlagsAutoComplete.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// TODO: Work on this component during the weekend
+const FlagsAutoComplete = () => {
+  return <div>This is where the flags autocomplete component will live</div>
+}
+
+export default FlagsAutoComplete
